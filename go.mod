9cdc73a0148b1ad3dd5edfa4ad96e87f81456dce
atlasdatatech <atlasdata@qq.com>
2020-09-07 23:41:14 +0800

    cesiumlab finished

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/flywave/go-3dtile v0.0.0-20200803051428-320006cd034e
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.2 // indirect
 	github.com/shaxbee/go-spatialite v0.0.0-20180425212100-9b4c81899e0e
 	github.com/sirupsen/logrus v1.6.0
+	gopkg.in/cheggaaa/pb.v1 v1.0.28
 )
--- main.go
+++ main.go
@@ -11,72 +11,35 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/cheggaaa/pb.v1"
 
 	tile3d "github.com/flywave/go-3dtile"
 	_ "github.com/shaxbee/go-spatialite"
 )
 
-var counter int
+var cnter int64
+var level int
 var basedir string
 var db *sql.DB
+var setlist []string
 var wg sync.WaitGroup
 var workers chan string
 
-func procTileset(url string) error {
-	file, err := os.OpenFile(url, os.O_RDWR, os.ModePerm)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	ts := tile3d.TilesetFromJson(file)
-	for i := range ts.Root.Children {
-		// procTile(url, &ts.Root.Children[i])
-		fmt.Print(i)
-	}
-	ts.Asset.GltfUpAxis = "Y"
-	transDefaut := true
-	for _, v := range ts.Root.Transform {
-		if v != 0 {
-			transDefaut = false
-		}
-	}
-	if transDefaut {
-		ts.Root.Transform = [16]float64{
-			1, 0, 0, 0,
-			0, 0, -1, 0,
-			0, 1, 0, 0,
-			0, 0, 0, 1}
-	}
-	tsjson, err := ts.ToJson()
-	if err != nil {
-		return err
-	}
-	err = file.Truncate(0)
-	if err != nil {
-		return err
-	}
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		return err
-	}
-	n, err := file.WriteString(tsjson)
-	if err != nil {
-		return err
-	}
-	counter++
-	log.Printf("rewrite %s ,  %d bytes~", url, n)
-	return nil
-}
+var bar *pb.ProgressBar
 
 //FetchTileset get tileset.json
-func FetchTileset(uri string) {
-	t := time.Now()
-	_, err := db.Exec("INSERT INTO tiles(u,t,p) values(?,?,?)", uri, 1, 0)
+func fetchTileset(uri string, depth bool) {
+	defer wg.Done()
+	defer func() {
+		<-workers
+	}()
 
+	time.Sleep(time.Second)
+	t := time.Now()
 	resp, err := http.Get(uri)
 	if err != nil {
 		log.Errorf("fetch :%s error, details: %s ~", uri, err)
@@ -109,26 +72,29 @@ func FetchTileset(uri string) {
 	if err != nil {
 		log.Warnf("write tileset %v ~", err)
 	}
-
-	// _, err = db.Exec("INSERT INTO tiles(u,t,p) values(?,?,?)", uri, 1, 0)
-	// 解析 计数 爬取subjson 写入列表
+	_, err = db.Exec("INSERT INTO tiles(u,t,s,c) values(?,?,?,?)", uri, 1, 0, 1)
+	if err != nil {
+		log.Errorf("insert tileset %s error", err)
+	}
 	bdReader := bytes.NewReader(body)
 	ts := tile3d.TilesetFromJson(bdReader)
+	atomic.StoreInt64(&cnter, 0)
 	for _, child := range ts.Root.Children {
-		procChild(uri, child)
+		procChild(uri, child, depth)
 	}
-	_, err = db.Exec("UPDATE tiles SET p = 1 WHERE u = ?", uri)
+
+	_, err = db.Exec("UPDATE tiles SET s = 1, c = ? WHERE u = ?", cnter, uri)
+	if err != nil {
+		log.Errorf("update tileset %s status error, detail: %v", uri, err)
+	}
 
 	secs := time.Since(t).Seconds()
 	fmt.Printf("tile %v, %.3fs, %.2f kb, %s ...\n", t, secs, float32(len(body))/1024.0, uri)
-	counter++
 }
 
 //FetchTile get tile
-func FetchTile(uri string) {
+func fetchTile(uri string) {
 	t := time.Now()
-	_, err := db.Exec("INSERT INTO tiles(u,t,p) values(?,?,?)", uri, 2, 0)
-
 	resp, err := http.Get(uri)
 	if err != nil {
 		log.Errorf("fetch :%s error, details: %s ~", uri, err)
@@ -162,33 +128,39 @@ func FetchTile(uri string) {
 	if err != nil {
 		log.Warnf("write tiles %v ~", err)
 	}
-	_, err = db.Exec("UPDATE tiles SET p = 1 WHERE u = ?", uri)
+
+	_, err = db.Exec("INSERT INTO tiles(u,t,s,c) values(?,?,?,?)", uri, 2, 1, 1)
+	if err != nil {
+		log.Errorf("insert %s error", err)
+	}
 
 	secs := time.Since(t).Seconds()
 	fmt.Printf("tile %v, %.3fs, %.2f kb, %s ...\n", t, secs, float32(len(body))/1024.0, uri)
-	counter++
 }
 
-func procChild(baseuri string, tile tile3d.Tile) {
+func procChild(baseuri string, tile tile3d.Tile, depth bool) {
+
 	if tile.Content != nil {
-		procContent(baseuri, tile.Content.Url)
+		procContent(baseuri, tile.Content.Url, depth)
 	}
 
 	for _, child := range tile.Children {
 
 		if child.Content != nil {
-			procContent(baseuri, child.Content.Url)
+			procContent(baseuri, child.Content.Url, depth)
 		}
 
 		if child.Children != nil {
 			for _, c := range child.Children {
-				procChild(baseuri, c)
+				procChild(baseuri, c, depth)
 			}
 		}
 	}
 }
 
-func procContent(buri, curi string) {
+func procContent(buri, curi string, depth bool) {
+	atomic.AddInt64(&cnter, 1)
+
 	bu, err := url.Parse(buri)
 	if err != nil {
 		log.Error(err)
@@ -201,106 +173,124 @@ func procContent(buri, curi string) {
 	ext := filepath.Ext(cu.Path)
 	switch strings.ToLower(ext) {
 	case ".json":
-		FetchTileset(uri)
+		if depth {
+			fetchTileset(uri, depth)
+		} else {
+			cnt := countTileset(uri)
+			_, err = db.Exec("INSERT INTO tiles(u,t,s,c) values(?,?,?,?)", uri, 1, 0, cnt)
+			if err != nil {
+				log.Errorf("insert tileset  %s error, details: %v", uri, err)
+			}
+		}
 	case ".b3dm", ".pnts", ".i3dm":
-		FetchTile(uri)
+		fetchTile(uri)
 	}
-	time.Sleep(time.Microsecond * 300)
+	bar.Increment()
 }
 
-func reFetchTile(uri string) {
-	defer wg.Done()
-	defer func() {
-		<-workers
-	}()
-
-	t := time.Now()
+//countTileset get tileset.json
+func countTileset(uri string) int {
+	var cnt int
 	resp, err := http.Get(uri)
 	if err != nil {
 		log.Errorf("fetch :%s error, details: %s ~", uri, err)
-		return
+		return cnt
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		log.Errorf("fetch %v tile error, status code: %d ~", uri, resp.StatusCode)
-		return
+		log.Errorf("fetch %v tileset error, status code: %d ~", uri, resp.StatusCode)
+		return cnt
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Errorf("read tile error ~ %s", err)
-		return
+		log.Errorf("read tileset error ~ %s", err)
+		return cnt
 	}
 	if len(body) == 0 {
-		log.Warnf("nil tile %v ~", uri)
-		return
+		log.Warnf("nil tileset %v ~", uri)
+		return cnt
 	}
-
-	u, err := url.Parse(uri)
-	if err != nil {
-		log.Errorf("parse :%s error, details: %s ~", uri, err)
-		return
+	bdReader := bytes.NewReader(body)
+	ts := tile3d.TilesetFromJson(bdReader)
+	for _, child := range ts.Root.Children {
+		cnt += countChild(child)
 	}
-	rawp := u.EscapedPath()
-	dir := filepath.Join(basedir, filepath.Dir(rawp))
-	os.MkdirAll(dir, os.ModePerm)
-	fileName := filepath.Join(basedir, rawp)
-	err = ioutil.WriteFile(fileName, body, os.ModePerm)
-	if err != nil {
-		log.Warnf("write tiles %v ~", err)
-	}
-
-	_, err = db.Exec("UPDATE tiles SET p = 1 WHERE u = ?", uri)
-
-	secs := time.Since(t).Seconds()
-	fmt.Printf("tile %v, %.3fs, %.2f kb, %s ...\n", t, secs, float32(len(body))/1024.0, uri)
-	counter++
-
+	return cnt
 }
 
-func reFetcher() {
-	rows, err := db.Query("SELECT u FROM tiles WHERE t=1 and p=0;")
+func countChild(tile tile3d.Tile) int {
+	var cnt int
+	if tile.Content != nil {
+		cnt++
+	}
+
+	for _, child := range tile.Children {
+		if child.Content != nil {
+			cnt++
+		}
+
+		if child.Children != nil {
+			for _, c := range child.Children {
+				cnt += countChild(c)
+			}
+		}
+	}
+	return cnt
+}
+
+func startFetcher(depth bool) {
+	rows, err := db.Query("SELECT u,c FROM tiles WHERE t=1 and s=0;")
 	if err != nil {
 		log.Fatal(err)
 	}
-	workers = make(chan string, 8)
+	defer rows.Close()
+
 	var url string
+	var total int
 	for rows.Next() {
-		rows.Scan(&url)
-		time.Sleep(time.Second)
+		cnt := 0
+		rows.Scan(&url, &cnt)
+		total += cnt
+		setlist = append(setlist, url)
+	}
+
+	prestr := ""
+	if depth {
+		log.Info("start dfs fetcher ~")
+		prestr = "dfs fetcher"
+	} else {
+		log.Infof("start bfs fetcher, level %d ~", level)
+		prestr = fmt.Sprintf("bfs fetcher, level %d", level)
+	}
+
+	bar = pb.New(total).Prefix(prestr).Postfix("\n")
+	// bar.SetRefreshRate(time.Second)
+	bar.Start()
+
+	workers = make(chan string, 8)
+
+	for _, url := range setlist {
 		select {
 		case workers <- url:
 			wg.Add(1)
-			go reFetchTile(url)
+			go fetchTileset(url, depth)
 		}
 	}
 	wg.Wait()
-}
-func upTileset() {
-	dir := filepath.Join(basedir, "ge", "BulkMetadata")
-	items, err := ioutil.ReadDir(dir)
-	if err != nil {
-		logrus.Error(err)
+	bar.FinishPrint("finished ~")
+	if !depth {
+		level++
+		startFetcher(depth)
 	}
-	baseurl := "http://lab.earthsdk.com/ge/BulkMetadata/"
-
-	// var files []string
-	for _, item := range items {
-		if item.IsDir() {
-			continue
-		}
-		_, err = db.Exec("UPDATE tiles SET p = 1 WHERE u = ?", baseurl+item.Name())
-		if err != nil {
-			logrus.Error(err)
-		}
-	}
-
 }
 
 func main() {
+
+	basedir = "./tiles"
 	uri := "http://lab.earthsdk.com/ge/tileset.json"
 	u, err := url.Parse(uri)
-	basedir = "d:/tiler"
 	os.MkdirAll(filepath.Join(basedir, filepath.Dir(u.Path)), os.ModePerm)
+
 	db, err = sql.Open("sqlite3", filepath.Join(basedir, filepath.Dir(u.Path), "tiles.db"))
 	if err != nil {
 		log.Fatal(err)
@@ -318,14 +308,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, err = db.Exec("create table if not exists tiles (u text,t integer, p integer);")
+	_, err = db.Exec("create table if not exists tiles (u text,t integer, s integer,c integer);")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	cnt := countTileset(uri)
+	_, err = db.Exec("INSERT INTO tiles(u,t,s,c) values(?,?,?,?)", uri, 1, 0, cnt)
+	if err != nil {
+		log.Fatal(err)
+	}
 	s := time.Now()
-	// upTileset()
-	reFetcher()
-	// FetchTileset(uri)
-	log.Printf("finished, proc %d files, times: %fs", counter, time.Since(s).Seconds())
+	startFetcher(false)
+	log.Printf("finished, consuming : %fs", time.Since(s).Seconds())
 }
